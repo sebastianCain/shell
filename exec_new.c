1d97e0143631f625348b0d3eac5e0c600540bf9b
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-12-01 23:11:57 -0500

    simple pipes work

--- exec_new.c
+++ exec_new.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "stringext.h"
 
@@ -86,7 +87,7 @@ int exec(){
 	if(strcmp(local_command[0], "cd") == 0){//this is dealt in parent
 	  return 0;
 	}
-
+	
 	//the stdout case
 	int pos;
 	pos=find(local_command, ">");
@@ -106,7 +107,182 @@ int exec(){
 
 	pos = find(local_command, "|");
 	if(pos != -1){
+	  //int status;
 
+	  char * hold[20];
+	  int e=0;
+	  while(local_command[e+pos+1]){
+	    hold[e]=local_command[pos+1];
+	    e++;
+	  }
+	  hold[e]=0;
+	  
+	  int fdx[2];
+	  pipe(fdx);
+
+	  int hold_in = dup(0);
+	  int hold_out = dup(1);	  
+
+	  dup2(1,hold_in);//redirect out to in
+	  //dup2(1, fd[1]);
+	  //dup2(0, fd[0]);
+
+	  //printf("[%s]\n", hold[0]);
+	  
+	  //execvp(hold[0], hold);
+	  
+	  int f;
+	  f=fork();
+
+	  if(f == 0){
+	    local_command[pos]=0;//cleave it off before the |
+	    printf("one\n");
+
+	    close(fdx[0]);//closes the read side
+	    close(1);
+	    printf("bump\n");
+	    dup2(fdx[1], 1);//puting fd[1] in place of 1
+
+	    execvp(local_command[0], local_command);
+	    return 0;
+	  }
+	  else{
+	    wait(&f);
+	    printf("heyo\n");
+	    close(fdx[1]);
+	    close(0);
+	    dup2(fdx[0], 0);
+	    execvp(hold[0], hold);
+	  }
+
+	  
+	  
+	  return 0;
+	  /*
+	  int status;
+
+	  char * hold_2[20];
+	  int e=0;
+	  while(local_command[e+pos+1]){
+	    hold_2[e]=local_command[pos+1];
+	    e++;
+	  }
+	  
+	  //int fd[2];
+	  //pipe(fd);
+
+	  int hold_in = dup(0);
+	  int hold_out = dup(1);
+
+	  int child;
+
+	  dup2(1,hold_in);//redirect out to in
+	  //dup2(1, fd[1]);
+	  //dup2(0, fd[0]);
+	  
+	  child=fork();
+
+	  if(child == 0){
+	    local_command[pos]=0;//cleave it off before the |
+	    printf("one\n");
+	    execvp(local_command[0], local_command);
+	    return 0;
+	  }
+	  else{
+	    wait(&status);
+	    char dest_a[100];
+	    fgets(dest_a, 100, stdin);
+	    char *newline = strstr(dest, "\n");
+	    *newline = 0;
+	    printf("[%s]\n", dest_a);
+	  }
+
+	  child = fork();
+
+	  if(child == 0){
+	    printf("two\n");
+	    execvp(hold_2[0], hold_2); 
+	    return 0;
+	  }
+	  else{
+	    wait(&status);
+	    dup2(hold_in, 0);
+	    dup2(hold_out, 1);
+	    //close(fd[0]);
+	    //close(fd[1]);
+	    return 0;
+	  }
+	   	  
+	  return 0;
+	  */
+	  /*
+	  int status;
+
+	  //moving the conetents;
+	  char * hold_2[20];
+	  int e=0;
+	  while(local_command[e+pos+1]){
+	    hold_2[e]=local_command[pos+1];
+	    e++;
+	  }
+
+	  int f1;
+	  int f2;
+
+	  int hold_in = dup(0);
+	  int hold_out = dup(1);
+
+	  int fd;
+	  
+	  f1=fork();
+	  if(f1 != 0){//child doesnt do this
+	    f2=fork();
+	  }
+	  
+	  
+	  if(f1==0){
+	    fd = getppid();
+	    dup2(1, fd);//f2);//put f2 in place of 1 or STD_OUT
+	    local_command[pos]=0;//cleave it off before the |
+
+	    printf("one\n");
+	    
+	    execvp(local_command[0], local_command);
+	    
+	  }
+	  else{
+	    wait(&status);
+	    //return the STD_OUT to its spot
+	    printf("what dup\n");
+	    dup2(hold_out, 1);
+	    
+	    if(f2==0){
+	      //set the STD_IN to pid f1
+	      //execvp(hold_2[0], hold_2);
+
+	      fd = getppid(); 
+	      dup2(0,fd);//f1);
+
+	      printf("two\n");
+	      printf("[%s]\n", hold_2[0]);
+	      
+	      execvp(hold_2[0], hold_2);
+
+	    }
+	    else{//where the process goes
+	      wait(&status);
+
+	      printf("three\n");
+	      
+	      //return  STD_IN to its place
+	      dup2(hold_in, 0);
+	      return 0;
+	    }
+	  }
+
+	  return 0;
+	  */
+	  /*
 	  int status;
 	  int fd = open("no_touch", O_WRONLY | O_CREAT | O_RDONLY, 0644);
 	  
@@ -170,7 +346,7 @@ int exec(){
 	  }
 	  
 	  return 0;
-	    
+	  */
 	}
 	
 	//all other cases
